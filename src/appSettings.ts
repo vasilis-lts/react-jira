dde63e19f29d215189ec561121304da792baecbc
vasilis_litsas <vasilis.litsas@architechts.gr>
2022-10-23 09:53:22 +0300

    updated app settings

--- src/appSettings.ts
+++ src/appSettings.ts
@@ -1,1 +1,6 @@
-export const BASE_URL = 'https://react-jira-backend-production.up.railway.app';
+console.log('Web-app environment: ' + process.env.NODE_ENV)
+
+export const BASE_URL =
+  process.env.NODE_ENV === 'development' ?
+    'http://localhost:5000' :
+    'https://react-jira-backend-production.up.railway.app';
