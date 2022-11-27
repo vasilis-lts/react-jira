b64902c795db6bd8b61ad1244e7c8349733e8891
vasilis_litsas <vasilis.litsas@architechts.gr>
2022-11-27 23:46:42 +0200

    changed prd backend url

--- src/appSettings.ts
+++ src/appSettings.ts
@@ -3,4 +3,5 @@ console.log('Web-app environment: ' + process.env.NODE_ENV)
 export const BASE_URL =
   process.env.NODE_ENV === 'development' ?
     'http://localhost:5000' :
-    'https://react-jira-backend-production.up.railway.app';
+    'https://backend.softnet.com.gr';
+    // 'https://react-jira-backend-production.up.railway.app';
