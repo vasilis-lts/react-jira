87d3d613c891f51cd8c4dbf1092ee193bc0025f8
vasilis_litsas <vasilis.litsas@architechts.gr>
2022-12-10 00:38:25 +0200

    changed app settings url

--- src/appSettings.ts
+++ src/appSettings.ts
@@ -1,7 +1,4 @@
 console.log('Web-app environment: ' + process.env.NODE_ENV)
 
-export const BASE_URL =
-  process.env.NODE_ENV === 'development' ?
-    'http://localhost:5000' :
-    'https://backend.softnet.com.gr';
-    // 'https://react-jira-backend-production.up.railway.app';
+export const BASE_URL = 'https://backend.softnet.com.gr'
+    // 'http://localhost:5000';
