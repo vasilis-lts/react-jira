a1a7a68f1491a9e0ddcc1f0eaa05248d3e5e5123
vasilis_litsas <vasilis.litsas@architechts.gr>
2022-12-11 11:23:58 +0200

    added env variables

--- src/appSettings.ts
+++ src/appSettings.ts
@@ -1,4 +1,3 @@
 console.log('Web-app environment: ' + process.env.NODE_ENV)
 
-// export const BASE_URL = 'https://backend.softnet.com.gr';
-export const BASE_URL = 'http://localhost:5000';
+export const BASE_URL = process.env.REACT_APP_ENV === "dev" ? 'http://localhost:5000' : 'https://backend.softnet.com.gr'
